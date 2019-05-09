d41127d84ca5d292a7c6e51fdd8fb6acc8be0717
zach <zhowes14@icloud.com>
2019-05-09 00:59:18 -0400

    The program now get the slider value and shows it in the alert box

--- bullseye/ViewController.swift
+++ bullseye/ViewController.swift
@@ -10,19 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var curentValue: Int = 0;
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     @IBAction func showAlert() {
-        let alert = UIAlertController(title: "Hello, World!", message: "This is my first app", preferredStyle: .alert )
         
-        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        let message = "The value of the slider is \(curentValue)"
+        
+        let alert = UIAlertController(title: "Hello, World!", message: message,
+                                      preferredStyle: .alert )
+        
+        let action = UIAlertAction(title: "Awesome",
+                                   style: .default, handler: nil)
         
         alert.addAction(action)
         
         present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func sliderMoved(_ slider: UISlider){
+        let roundedValue = slider.value.rounded()
+        curentValue = Int(roundedValue)
     }
 
 }
